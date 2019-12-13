d2d300332e93746c17546a0584059e365e4f19b0
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2019-12-13 21:18:54 +0300

    first commit

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-POLLING = False
 TOKEN = '1018631586:AAH0_7YW69IolgVBW9HfFgr9bMiXgzwz7vYz'
 BOT_NAME = 'Фаг'
 BOT_LOGIN = 'Faggoatbot'
@@ -10,4 +9,4 @@ WEBHOOK_PORT = 7773
 WEBHOOK_LISTEN = '127.0.0.1'
 
 WEBHOOK_SSL_CERT = '/home/godfather/ssl/ssl-bundle.crt'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = '/home/godfather/ssl/thegangsters.key'  # Path to the ssl private key
+WEBHOOK_SSL_PRIV = '/home/godfather/ssl/thegangsters.key'  # Path to the ssl private key
